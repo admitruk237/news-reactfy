bf1119d85e4ab32ad819b9daff5d5243e3a1c71b
Andrii Dmytruk <admitruk237@gmail.com>
2024-12-06 14:45:48 +0100

    add context and change theme

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,25 @@
 import { formatDate } from '../../helpers/formatDate';
+import { TiAdjustBrightness, TiWeatherNight } from 'react-icons/ti';
 import styles from './styles.module.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function Header() {
+  const { isDark, toggleTheme } = useTheme();
   return (
-    <header className={styles.header}>
-      <h1 className={styles.title}>NEWS REACTFY</h1>
-      <p className={styles.date}>{formatDate(new Date())}</p>
+    <header
+      className={`${styles.header} ${isDark ? styles.dark : styles.light}`}
+    >
+      <div className={styles.info}>
+        <h1 className={styles.title}>NEWS REACTFY</h1>
+        <p className={styles.date}>{formatDate(new Date())}</p>
+      </div>
+      <div className={styles.swichTheme} onClick={toggleTheme}>
+        {isDark ? (
+          <TiAdjustBrightness className={styles.day} />
+        ) : (
+          <TiWeatherNight className={styles.night} />
+        )}
+      </div>
     </header>
   );
 }
