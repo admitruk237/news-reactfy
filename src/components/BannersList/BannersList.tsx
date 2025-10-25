7b73575340bf17902f1f6fc5ce2e6c4d37117e85
Andrii Dmtryk <admitruk237@gmail.com>
2025-10-25 23:13:51 +0200

    fix: in mobile show only one banner

--- src/components/BannersList/BannersList.tsx
+++ src/components/BannersList/BannersList.tsx
@@ -1,20 +1,37 @@
-import styles from './styles.module.css';
-import NewsBanner from '../../NewsBanner/NewsBanner';
-import withSkeleton from '../../helpers/hocs/withSkeleton';
-import { INews } from '../../interfaces';
+import { useState, useEffect } from 'react'
+import styles from './styles.module.css'
+import NewsBanner from '../../NewsBanner/NewsBanner'
+import withSkeleton from '../../helpers/hocs/withSkeleton'
+import { INews } from '../../interfaces'
 
 interface Props {
-  banners?: INews[] | null;
+  banners?: INews[] | null
 }
 
 function BannersList({ banners }: Props) {
+  const [isDesktop, setIsDesktop] = useState(false)
+
+  useEffect(() => {
+    const checkScreen = () => setIsDesktop(window.innerWidth > 768)
+
+    checkScreen()
+    window.addEventListener('resize', checkScreen)
+
+    return () => window.removeEventListener('resize', checkScreen)
+  }, [])
+
   return (
     <ul className={styles.banners}>
-      {banners?.map((banner) => {
-        return <NewsBanner key={banner.id} item={banner} />;
-      })}
+      {isDesktop
+        ? banners?.map((banner) => (
+            <NewsBanner
+              key={banner.id}
+              item={banner}
+            />
+          ))
+        : banners && banners[0] && <NewsBanner item={banners[0]} />}
     </ul>
-  );
+  )
 }
 
 const BannersListWithSkeleton = withSkeleton<Props>(
@@ -22,6 +39,6 @@ const BannersListWithSkeleton = withSkeleton<Props>(
   'banner',
   10,
   'row'
-);
+)
 
-export default BannersListWithSkeleton;
+export default BannersListWithSkeleton
